ed881fd11e86738a3e87a74b7a6bd2ba02684ebd
aftid <alexandremiquel.frauamar.practicas@telefonica.com>
2026-01-27 12:20:42 +0100

    Add files via upload

---
+++ iperf/script-tmux.sh
@@ -0,0 +1,20 @@
+# Copyright 2026 Telefónica Innovación Digital (alexandremiquel.frauamar.practicas@telefonica.com, antonio.pastorperales@telefonica.com)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#!/bin/bash
+
+# Start Flask server in a tmux session to perform the iperf petition
+tmux new-session -d -s iperf_session "source flaskenv/bin/activate && python3 service.py"
+
+# Start Flask server in a tmux session to expose the iperf metrics file
+tmux new-session -d -s expose_session "source flaskenv/bin/activate && python3 metrics_service.py"
+
+# Change the default route to go via 172.20.0.50
+ip route change default via 172.20.0.50
+
+exec "$@"
---
+++ secgw2/script-tmux.sh
@@ -0,0 +1,24 @@
+# Copyright 2026 Telefónica Innovación Digital (alexandremiquel.frauamar.practicas@telefonica.com, antonio.pastorperales@telefonica.com)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#!/bin/bash
+
+# Start Flask server in a tmux session
+tmux new-session -d -s flask_session "source flaskenv/bin/activate && python3 service.py"
+
+# Start Strongswan's charon in a tmux session
+tmux new-session -d -s charon_session "cd /usr/local/libexec/ipsec && ./charon"
+
+# Loads Strongswan's configuration and initiates IPsec tunnel
+sleep 0.5
+swanctl --load-all
+swanctl -i --child net-net
+swanctl -t --child net-net
+swanctl -i --child net-net
+
+exec "$@"
